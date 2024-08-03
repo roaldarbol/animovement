6eda10efb405b202701b0e87976403c7eede7c34
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-08-03 15:45:45 +0100

    Remove older sample data

--- data-raw/sensor_data.R
+++
@@ -1,3 +0,0 @@
-filepaths <- list.files(pattern = ".csv", recursive = TRUE)
-sensor_data <- read_trackball_data(filepaths, configuration = "open", format = "csv")
-usethis::use_data(sensor_data, overwrite = TRUE)
--- data-raw/trackball_filenames.R
+++
@@ -1,2 +0,0 @@
-trackball_filenames <- readRDS("beetle_trackball_filenames.rds")
-usethis::use_data(trackball_filenames, overwrite = TRUE)
