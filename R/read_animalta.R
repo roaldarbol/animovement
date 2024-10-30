f95db97b5867d6453258dc1c095ca2664dc9e493
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-10-30 09:50:37 +0100

    Begin on animalTA

--- R/read_animalta.R
+++ R/read_animalta.R
@@ -8,5 +8,17 @@
 #' @return a movement dataframe
 #' @export
 read_animalta <- function(path) {
+  data <- vroom::vroom(
+    path,
+    delim = ";",
+    show_col_types = FALSE
+  ) |>
+    janitor::clean_names() |>
+    dplyr::mutate(frame = as.numeric(frame),
+                  time = as.numeric(time)) |>
+    dplyr::rename(individual = "ind") |>
+    dplyr::select(-c("frame", "arena"))
   cli::cli_abort("`read_animalta` has not yet been implemented. Coming soon!")
 }
+# path <- here::here("tests", "data", "animalta", "single_individual_multi_arena.csv")
+# path <- here::here("tests", "data", "animalta", "variable_individuals_single_arena.csv")
