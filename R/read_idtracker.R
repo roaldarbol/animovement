04215b075c5b1cf9534c942de79da458d7e0e06a
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-10-30 16:16:30 +0100

    Based on earlier code, not tested

--- R/read_idtracker.R
+++ R/read_idtracker.R
@@ -3,10 +3,32 @@
 #' @description
 #' `r lifecycle::badge('experimental')`
 #'
-#' @param data An idtracker.ai data frame
+#' @param path Path to an idtracker.ai data frame
+#'
+#' @import dplyr
+#' @import tidyr
+#' @importFrom vroom vroom
+#' @importFrom janitor clean_names
 #'
 #' @return a movement dataframe
 #' @export
-read_idtracker <- function(data) {
-  cli::cli_abort("`read_idtracker` has not yet been implemented. Coming soon!")
+read_idtracker <- function(path) {
+  data <- vroom::vroom(
+    path,
+    delim = ",",
+    show_col_types = FALSE
+  ) |>
+    suppressMessages() |>
+    janitor::clean_names() |>
+    tidyr::pivot_longer(cols = !1,
+                 values_to = "value",
+                 names_to = c("axis", "id"),
+                 names_pattern = "(.)(.)") |>
+    dplyr::mutate(id = as.factor(.data$id)) |>
+    tidyr::pivot_wider(id_cols = c("id", "seconds"),
+                values_from = "value",
+                names_from = "axis") |>
+    dplyr::rename(time = "seconds")
+
+  return(data)
 }
