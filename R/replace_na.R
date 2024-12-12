143d137d8a77af1e5a5ff13302d10eb87ba07340
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-12-12 11:41:41 +0100

    Allow na_interpolation to return an unfiltered data frame with a warning

--- R/replace_na.R
+++ R/replace_na.R
@@ -197,7 +197,8 @@ na_interpolation <- function(x, option = "linear", maxgap = Inf, ...) {
 
     # 1.3 Check for algorithm specific minimum amount of non-NA values
     if (sum(!missindx) < 2) {
-      stop("At least 2 non-NA data points required in the time series to apply na_interpolation.")
+      skip <- TRUE
+      warning("At least 2 non-NA data points required in the time series to apply na_interpolation.")
     }
 
     # 1.4 Checks and corrections for wrong data dimension
@@ -226,97 +227,98 @@ na_interpolation <- function(x, option = "linear", maxgap = Inf, ...) {
     ##
     ## 2. Imputation Code
     ##
+    if (is_false(skip)){
+      n <- length(data)
 
-    n <- length(data)
+      allindx <- 1:n
+      indx <- allindx[!missindx]
 
-    allindx <- 1:n
-    indx <- allindx[!missindx]
+      data_vec <- as.vector(data)
 
-    data_vec <- as.vector(data)
-
-    # Linear Interpolation
-    if (option == "linear") {
-      # Check if 'rule' is used in function call, to allow parameter pass through for rule
-      # Needed since parameter pass through via (...) to approx does not work, when value for 'rule' is also set in the code.
-      if (methods::hasArg(rule)) {
-        interp <- stats::approx(indx, data_vec[indx], 1:n, ...)$y
+      # Linear Interpolation
+      if (option == "linear") {
+        # Check if 'rule' is used in function call, to allow parameter pass through for rule
+        # Needed since parameter pass through via (...) to approx does not work, when value for 'rule' is also set in the code.
+        if (methods::hasArg(rule)) {
+          interp <- stats::approx(indx, data_vec[indx], 1:n, ...)$y
+        }
+        else {
+          interp <- stats::approx(indx, data_vec[indx], 1:n, rule = 2, ...)$y
+        }
       }
+      # Spline Interpolation
+      else if (option == "spline") {
+        interp <- stats::spline(indx, data_vec[indx], n = n, ...)$y
+      }
+      # Stineman Interpolation
+      else if (option == "stine") {
+        interp <- stinepack::stinterp(indx, data_vec[indx], 1:n, ...)$y
+        # avoid NAs at the beginning and end of series // same behavior like
+        # for approx with rule = 2.
+        if (any(is.na(interp))) {
+          interp <- collapse::na_locf(interp) #na_remaining = "rev")
+        }
+      }
+      # Wrong parameter option
       else {
-        interp <- stats::approx(indx, data_vec[indx], 1:n, rule = 2, ...)$y
+        stop("Wrong parameter 'option' given. Value must be either 'linear', 'spline' or 'stine'.")
       }
-    }
-    # Spline Interpolation
-    else if (option == "spline") {
-      interp <- stats::spline(indx, data_vec[indx], n = n, ...)$y
-    }
-    # Stineman Interpolation
-    else if (option == "stine") {
-      interp <- stinepack::stinterp(indx, data_vec[indx], 1:n, ...)$y
-      # avoid NAs at the beginning and end of series // same behavior like
-      # for approx with rule = 2.
-      if (any(is.na(interp))) {
-        interp <- collapse::na_locf(interp) #na_remaining = "rev")
+
+      # Merge interpolated values back into original time series
+      data[missindx] <- interp[missindx]
+
+      ##
+      ## End Imputation Code
+      ##
+
+
+      ##
+      ## 3. Post Processing
+      ##
+
+      # 3.1 Check for Maxgap option
+
+      # If maxgap = Inf then do nothing and when maxgap is lower than 0
+      if (is.finite(maxgap) && maxgap >= 0) {
+
+        # Get logical vector of the time series via is.na() and then get the
+        # run-length encoding of it. The run-length encoding describes how long
+        # the runs of FALSE and TRUE are
+        rlencoding <- rle(is.na(x))
+
+        # Runs smaller than maxgap (which shall still be imputed) are set FALSE
+        rlencoding$values[rlencoding$lengths <= maxgap] <- FALSE
+
+        # The original vector is being reconstructed by reverse.rls, only now the
+        # longer runs are replaced now in the logical vector derived from is.na()
+        # in the beginning all former NAs that are > maxgap are also FALSE
+        en <- inverse.rle(rlencoding)
+
+        # Set all positions in the imputed series with gaps > maxgap to NA
+        # (info from en vector)
+        data[en == TRUE] <- NA
       }
+
+      ##
+      ## End Post Processing
+      ##
+
+
+      ##
+      ## 4. Final Output Formatting
+      ##
+
+      # Give back the object originally supplied to the function
+      # (necessary for multivariate input with only 1 column)
+      if (!is.null(dim(x)[2])) {
+        x[, 1] <- data
+        return(x)
+      }
+
+      ##
+      ## End Final Output Formatting
+      ##
     }
-    # Wrong parameter option
-    else {
-      stop("Wrong parameter 'option' given. Value must be either 'linear', 'spline' or 'stine'.")
-    }
-
-    # Merge interpolated values back into original time series
-    data[missindx] <- interp[missindx]
-
-    ##
-    ## End Imputation Code
-    ##
-
-
-    ##
-    ## 3. Post Processing
-    ##
-
-    # 3.1 Check for Maxgap option
-
-    # If maxgap = Inf then do nothing and when maxgap is lower than 0
-    if (is.finite(maxgap) && maxgap >= 0) {
-
-      # Get logical vector of the time series via is.na() and then get the
-      # run-length encoding of it. The run-length encoding describes how long
-      # the runs of FALSE and TRUE are
-      rlencoding <- rle(is.na(x))
-
-      # Runs smaller than maxgap (which shall still be imputed) are set FALSE
-      rlencoding$values[rlencoding$lengths <= maxgap] <- FALSE
-
-      # The original vector is being reconstructed by reverse.rls, only now the
-      # longer runs are replaced now in the logical vector derived from is.na()
-      # in the beginning all former NAs that are > maxgap are also FALSE
-      en <- inverse.rle(rlencoding)
-
-      # Set all positions in the imputed series with gaps > maxgap to NA
-      # (info from en vector)
-      data[en == TRUE] <- NA
-    }
-
-    ##
-    ## End Post Processing
-    ##
-
-
-    ##
-    ## 4. Final Output Formatting
-    ##
-
-    # Give back the object originally supplied to the function
-    # (necessary for multivariate input with only 1 column)
-    if (!is.null(dim(x)[2])) {
-      x[, 1] <- data
-      return(x)
-    }
-
-    ##
-    ## End Final Output Formatting
-    ##
 
     return(data)
   }
